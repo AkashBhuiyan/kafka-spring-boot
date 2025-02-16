99edbac6a155dab5e3c1f743ef086d777d8ed60c
Akash <akash.bhuiyan@gmail.com>
2025-02-16 18:19:46 +0100

    Producer and Consumer added for Wikimedia Stream data

---
+++ consumer/src/test/java/com/akash/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
